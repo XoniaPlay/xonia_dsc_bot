3e1993222f31458bf18e1c9877139aca6ba610b6
dada513 <dada513@protonmail.com>
2021-08-21 19:21:38 +0200

    Revert "remove supid url check"
    This reverts commit 093648c3c72b6a7cea93df329b95fadf2b792a9b.

--- src/events/MessageEvent.js
+++ src/events/MessageEvent.js
@@ -1,47 +1,28 @@
 // https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-message
-const BaseEvent = require("../utils/structures/BaseEvent");
+const BaseEvent = require('../utils/structures/BaseEvent');
 module.exports = class MessageEvent extends BaseEvent {
   constructor() {
-    super("message");
+    super('message');
   }
+  
+  async run(client, message){
+    if(message.author.id === '551819471149465631'){
+      
+    }
+    else if(message.author.id === '801401072168992768'){
+      
+    }
+    else{
+      let blacklistedwords = ['fuck', 'dick', 'asshole', 'bitch', 'retard', 'bastard', 'cunt', 'ass', 'cum', 'sex', 'sexy', 'https://', 'http://', 'grabify.link', 'discord.gift/', 'iplogger.org', 'pornhub.com', 'hentaihaven.xxx', 'hentaihaven.red']
 
-  async run(client, message) {
-    if (message.author.id === "551819471149465631") {
-    } else if (message.author.id === "801401072168992768") {
-    } else {
-      let blacklistedwords = [
-        "fuck",
-        "dick",
-        "asshole",
-        "bitch",
-        "retard",
-        "bastard",
-        "cunt",
-        "ass",
-        "cum",
-        "sex",
-        "sexy",
-        "grabify.link",
-        "discord.gift/",
-        "iplogger.org",
-        "pornhub.com",
-        "hentaihaven.xxx",
-        "hentaihaven.red",
-      ];
-
-      let badworddetected = false;
-      for (var i in blacklistedwords) {
-        if (
-          message.content
-            .toLowerCase()
-            .includes(blacklistedwords[i].toLowerCase())
-        )
-          badworddetected = true;
-      }
-      if (badworddetected) {
-        message.delete();
-        message.channel.send(`Bad word detected!`);
-      }
+    let badworddetected = false;
+    for (var i in blacklistedwords) {
+      if(message.content.toLowerCase().includes(blacklistedwords[i].toLowerCase())) badworddetected = true;
+    }
+    if(badworddetected){
+      message.delete()
+      message.channel.send(`Bad word detected!`)
+    }
     }
   }
-};
+}
